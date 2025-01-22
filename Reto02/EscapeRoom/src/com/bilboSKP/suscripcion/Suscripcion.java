afe0bc3d422a94e3f0797861ef7c1c17329d3528
Unai <unai.carbajo.amundarain@gmail.com>
2025-01-22 18:38:00 +0100

    Corrección de errores

--- Reto02/EscapeRoom/src/com/bilboSKP/suscripcion/Suscripcion.java
+++ Reto02/EscapeRoom/src/com/bilboSKP/suscripcion/Suscripcion.java
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-package com.bilboSKP.suscripcion;
-
-import java.time.LocalDate;
-import com.bilboSKP.centroEscolar.centroEscolar;
-
-public class Suscripcion {
-
-	private int iDSuscripcion;
-	private boolean estado = false;
-	private LocalDate fechaActivacion = LocalDate.now();
-	private String centroID;
-	
-	public Suscripcion(centroEscolar centro) {
-		super();
-		this.centroID = centro.getCif();
-	}
-	
-	
-	public int getiDSuscripcion() {
-		return iDSuscripcion;
-	}
-	public void setiDSuscripcion(int iDSuscripcion) {
-		this.iDSuscripcion = iDSuscripcion;
-	}
-	public boolean isEstado() {
-		return estado;
-	}
-	public void setEstado(boolean estado) {
-		this.estado = estado;
-	}
-	public LocalDate getFechaActivacion() {
-		return fechaActivacion;
-	}
-	public void setFechaActivacion(LocalDate fechaActivacion) {
-		this.fechaActivacion = fechaActivacion;
-	}
-	public String getCentroID() {
-		return centroID;
-	}
-	public void setCentroID(centroEscolar centroID) {
-		this.centroID = centroID.getCif();
-	}
-	public void setCentroID(String centroID) {
-		this.centroID = centroID;
-	}
-	
-}
-=======
 package com.bilboSKP.suscripcion;
 
 import java.time.LocalDate;
@@ -97,4 +48,3 @@ public class Suscripcion {
 	}
 	
 }
->>>>>>> aa7fccbd6005ef566e0da8d9027c460837b8e2c6
--- Reto02/EscapeRoom/src/com/bilboSKP/view/App.java
+++ Reto02/EscapeRoom/src/com/bilboSKP/view/App.java
@@ -1,13 +1,9 @@
 package com.bilboSKP.view;
 
-import com.bilboSKP.UI.PantallaInicioFrame;
-
 public class App {
 
 	public static void main(String[] args) {
 	
-		PantallaInicioFrame ventana = new PantallaInicioFrame();
-		ventana.setVisible(true);
 	}
 
 }
