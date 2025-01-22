b5703e00bd624f1f9bed64050f8e0d13697ff501
deiner <deiner>
2025-01-22 09:33:52 +0100

    Ajustes Deiner

--- Reto02/EscapeRoom/src/com/bilboSKP/UI/PantallaInicioFrame.java
+++ Reto02/EscapeRoom/src/com/bilboSKP/UI/PantallaInicioFrame.java
@@ -3,13 +3,17 @@ package com.bilboSKP.UI;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class PantallaInicioFrame extends JFrame {
 
 	private JPanel contentPane;
+	private ImageIcon originalIcon;
 
 	public PantallaInicioFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,7 +21,16 @@ public class PantallaInicioFrame extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel JLabelImagen = new JLabel("");
+		JLabelImagen.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabelImagen.setIcon(new ImageIcon("D:\\Proyecto2\\DALL\u00B7E 2025-01-21 12.59.33 - A creative and dramatic design for a start screen of an escape room themed around bullying and witnessing. The central focus is a wooden door with a d.jpg"));
+		JLabelImagen.setEnabled(false);
+		contentPane.add(JLabelImagen, BorderLayout.CENTER);
+		
+		originalIcon = new ImageIcon ("D:\\Proyecto2\\DALL\u00B7E 2025-01-21 12.59.33 - A creative and dramatic design for a start screen of an escape room themed around bullying and witnessing. The central focus is a wooden door with a d.jpg");
+
 	}
 
 }
