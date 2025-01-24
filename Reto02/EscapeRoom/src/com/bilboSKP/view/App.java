f58036ef0a3ea076de02f1707488bd678db9e666
unai <unai>
2025-01-24 13:54:40 +0100

    Corrección de errores

--- Reto02/EscapeRoom/src/com/bilboSKP/view/App.java
+++ Reto02/EscapeRoom/src/com/bilboSKP/view/App.java
@@ -4,28 +4,17 @@ import com.bilboSKP.UI.ComprarCuponesFrame;
 import com.bilboSKP.UI.IntroducirCodigoFrame;
 import com.bilboSKP.UI.PantallaInicioFrame;
 import com.bilboSKP.centroEscolar.CentroEscolar;
-<<<<<<< HEAD
+
 import com.bilboSKP.partida.UI.BPasilloFrame;
 import com.bilboSKP.partida.UI.BSalaDeImpresoraFrame;
-=======
-import com.bilboSKP.partida.UI.EntradaJuego;
->>>>>>> b07f297af9cf1d1648201678b5e03c6c8eb6263e
 
 public class App {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
-		BSalaDeImpresoraFrame ventana = new BSalaDeImpresoraFrame();
+		IntroducirCodigoFrame ventana = new IntroducirCodigoFrame();
 		ventana.setVisible(true);
-=======
-/*PantallaInicioFrame ventana = new PantallaInicioFrame();
-		ventana.setVisible(true);*/
-		
-		EntradaJuego ventana2 = new EntradaJuego();
-		ventana2.setVisible(true);
-	
->>>>>>> b07f297af9cf1d1648201678b5e03c6c8eb6263e
+
 	}
 
 }
