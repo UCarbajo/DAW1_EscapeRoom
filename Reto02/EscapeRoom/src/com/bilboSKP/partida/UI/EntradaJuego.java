0c4bf593dc144e0fd1f639749b9bbaea00e3bfa1
Deiner <deinerjavieruyuquipa@gmail.com>
2025-01-28 13:47:41 +0100

    Deiner 28

--- Reto02/EscapeRoom/src/com/bilboSKP/partida/UI/EntradaJuego.java
+++ Reto02/EscapeRoom/src/com/bilboSKP/partida/UI/EntradaJuego.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,9 +12,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JTextPane;
-import java.awt.Color;
-import java.awt.SystemColor;
-import javax.swing.JScrollBar;
+import javax.swing.border.LineBorder;
+import java.awt.Insets;
 
 public class EntradaJuego extends JFrame {
 
@@ -27,29 +27,29 @@ public class EntradaJuego extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		ImageIcon icon = new ImageIcon("entradaCole.jpeg");
+
+		// Aplicar borde redondeado
+		int borderRadius = 20; // Radio de las esquinas redondeadas
 		
 		JTextPane txtpnenEstaEscuela = new JTextPane();
 		txtpnenEstaEscuela.setEditable(false);
-		txtpnenEstaEscuela.setBackground(SystemColor.controlHighlight);
-		txtpnenEstaEscuela.setText("\"En esta escuela, un estudiante llamado Juan sufri\u00F3 en silencio. Sus huellas a\u00FAn est\u00E1n aqu\u00ED, esperando que alguien escuche su historia. Sigue las pistas para entender lo que ocurri\u00F3 y c\u00F3mo evitar que se repita.\"");
-		txtpnenEstaEscuela.setBounds(424, 363, 477, 126);
+		txtpnenEstaEscuela.setText("\"En esta escuela, un estudiante llamado Juan sufrió en silencio. Sus huellas aún están aquí, esperando que alguien escuche su historia. Sigue las pistas para entender lo que ocurrió y cómo evitar que se repita.\"");
+		txtpnenEstaEscuela.setBounds(405, 363, 521, 142);
 		contentPane.add(txtpnenEstaEscuela);
-		txtpnenEstaEscuela.setOpaque(false);
-		txtpnenEstaEscuela.setBackground(new Color(0, 0, 0, 150));
-		txtpnenEstaEscuela.setForeground(Color.WHITE);
-		txtpnenEstaEscuela.getFont();
-		txtpnenEstaEscuela.setFont(new Font("Arial", Font.PLAIN, 20));
 		
-		/*JLabel lblTransparente = new JLabel("Hola");
-		lblTransparente.setBounds(423, 367, 478, 126);
-		lblTransparente.setBackground(new Color(100, 100, 100, 150)); 
-		lblTransparente.setOpaque(true);
-		lblTransparente.setForeground(Color.BLACK);*/
-		
-		JLabel lblFondoTransparente = new JLabel("hola");
-		lblFondoTransparente.setBounds(419, 363, 482, 126);
-		contentPane.add(lblFondoTransparente);
-		
+				// Fondo blanco con algo de opacidad (semitransparente)
+				txtpnenEstaEscuela.setOpaque(true);
+				txtpnenEstaEscuela.setBackground(new Color(255, 255, 255, 180)); // Blanco con opacidad (RGBA)
+				txtpnenEstaEscuela.setForeground(Color.BLACK); // Color del texto
+				txtpnenEstaEscuela.setFont(new Font("Arial", Font.PLAIN, 20));
+				// Establecer un borde de color negro y grosor de 2px
+				txtpnenEstaEscuela.setBorder(new LineBorder(Color.BLACK, 2) {
+					@Override
+					public Insets getBorderInsets(java.awt.Component c) {
+						return new Insets(borderRadius, borderRadius, borderRadius, borderRadius); // Espaciado de borde
+					}
+				});
+
 		JLabel lblEntradaCole = new JLabel("");
 		lblEntradaCole.setIcon(new ImageIcon("imagenes/entradaCole.jpeg"));
 		lblEntradaCole.setBounds(0, 0, 1280, 692);
@@ -57,12 +57,10 @@ public class EntradaJuego extends JFrame {
 		lblEntradaCole.setBackground(new Color(100, 100, 100, 150));
 		lblEntradaCole.setOpaque(true);
 		lblEntradaCole.setForeground(Color.black);
-		
 
 		Image img = icon.getImage();
-
-		Image scaledImg = img.getScaledInstance(1280,720 , Image.SCALE_SMOOTH);
-
+		Image scaledImg = img.getScaledInstance(1280, 720, Image.SCALE_SMOOTH);
 		ImageIcon scaledIcon = new ImageIcon(scaledImg);
 	}
 }
+
--- Reto02/EscapeRoom/src/com/bilboSKP/view/App.java
+++ Reto02/EscapeRoom/src/com/bilboSKP/view/App.java
@@ -4,18 +4,23 @@ import com.bilboSKP.UI.ComprarCuponesFrame;
 import com.bilboSKP.UI.IntroducirCodigoFrame;
 import com.bilboSKP.UI.PantallaInicioFrame;
 import com.bilboSKP.centroEscolar.CentroEscolar;
-
+import com.bilboSKP.partida.UI.APrimeraPueba;
 import com.bilboSKP.partida.UI.BPasilloFrame;
 import com.bilboSKP.partida.UI.BSalaDeImpresoraFrame;
 import com.bilboSKP.partida.UI.BSegundaPruebaFrame;
+import com.bilboSKP.partida.UI.EntradaJuego;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		BSegundaPruebaFrame ventana = new BSegundaPruebaFrame();
+		/*BSegundaPruebaFrame ventana = new BSegundaPruebaFrame();
+		ventana.setVisible(true);*/
+		
+		APrimeraPueba ventana = new APrimeraPueba();
 		ventana.setVisible(true);
-
+		
+	
 	}
 
 }
